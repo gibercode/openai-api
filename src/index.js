9cd0c1eb7518f98414a28d47ffe167999e81be45
gibercode <giberlara@gmail.com>
2023-07-11 23:56:23 -0400

    structure changed

--- src/index.js
+++ src/index.js
@@ -2,33 +2,37 @@ const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
 const app = express();
 const port = 5001;
+require('dotenv').config();
+
 const configuration = new Configuration({
   organization: 'org-RRstoS5NJNSxt2tl2muw27h4',
-  apiKey: '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'
+  apiKey: process.env.API_URL
 });
 
+app.use(express.json())
 const openai = new OpenAIApi(configuration);
-app.use(express.json())  
 
 app.post("/", async (req, res) => {
-  const { body } = req;
+  const { text } = req.body;
   try {
     const response = await openai.createCompletion({
       model:"text-davinci-003",
-      prompt: body?.text,
-      temperature: 1,
-      max_tokens: 500,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0
+      prompt:text,
+      temperature:1,
+      max_tokens:64,
+      top_p:1.0,
+      frequency_penalty:0.0,
+      presence_penalty:0.0
     });
 
-    res.json({ text: response?.data?.choices?.[0]?.text || '' });
+    res.send({ response: response.data.choices[0].text });
+    return {}
   } catch (err) {
+		console.log('error');
     res.status(500).send('Something broke!')
   }
 });
 
 app.listen(port, () => {
-  console.log(`Api running on ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
