1b03173f6e2aa3c4d9728d95f1c206c93813bbba
gibercode <giberlara@gmail.com>
2023-07-11 23:07:53 -0400

    error fixed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const http = require("http");
 const { Configuration, OpenAIApi } = require("openai");
 const app = express();
 const port = 5001;
-
 const configuration = new Configuration({
   organization: 'org-RRstoS5NJNSxt2tl2muw27h4',
   apiKey: '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'
@@ -17,16 +15,15 @@ app.post("/", async (req, res) => {
   try {
     const response = await openai.createCompletion({
       model:"text-davinci-003",
-      prompt: body.text,
-      temperature:1,
+      prompt: body?.text,
+      temperature: 1,
       max_tokens: 500,
       top_p: 1.0,
       frequency_penalty: 0.0,
       presence_penalty: 0.0
     });
 
-    res.json({ text: response?.data?.choices?[0]?.text || '' })
-
+    res.json({ text: response?.data?.choices?.[0]?.text || '' });
   } catch (err) {
     res.status(500).send('Something broke!')
   }
