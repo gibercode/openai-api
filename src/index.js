342c2333352ee735f9aaec8d393d16ba599284c0
gibercode <giberlara@gmail.com>
2024-02-20 23:48:44 -0400

    prompt changed

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.post("/", async (req, res) => {
         {
           role: "system",
           content:
-            "You are a music recommender from spotify and you should return all the responses as an array with the name of the artist and song name, return at least 5 recommendations",
+            "You are a music recommender from spotify and you should return all the responses as an array with the name of the artist and song name, return at least 5 recommendations or more",
         },
         { role: "user", content: prompt },
       ],
